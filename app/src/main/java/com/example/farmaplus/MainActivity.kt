9fca6dfee5112b544c140bf1e8c8ce93ef5ca45b
patrie <pmespert@gmail.com>
2022-10-22 13:38:48 +0200

    Configure retrofit and test it with a first request

--- app/src/main/java/com/example/farmaplus/MainActivity.kt
+++ app/src/main/java/com/example/farmaplus/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.farmaplus
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+            val call = RemoteConnection.service.getMedicines(1,1,1)
+            val result = call.body()
+            if(call.isSuccessful){
+                val medicines = result?.medicines ?: emptyList()
+                Log.d("Medicines", medicines.toString())
+            }else{
+                showError()
+            }
+
+        }
     }
+
+
+    private fun showError(){
+        toast("Ha ocurrido un error.")
+    }
+
+
+    /*private fun parseDateExample(epochTimestamp:Long) {
+        val timeStamp = Timestamp(epochTimestamp)
+        val dateToParse = Date(timeStamp.time)
+//        Log.d("Mg-date", dateToParse.toString())
+        val dateFormat = "EEE d MMM HH:mm:ss z yyyy"
+        val sdf = SimpleDateFormat(dateFormat, Locale.getDefault())
+        try {
+            val date: Date = sdf.parse(dateToParse.toString())
+            Log.d("Mg-date", date.toString())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }*/
 }
---
+++ app/src/main/java/com/example/farmaplus/RemoteResult.kt
@@ -0,0 +1,103 @@
+package com.example.farmaplus
+
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+data class Medicine(
+    @SerializedName("nregistro") val registerNumber: String,
+    @SerializedName("nombre") val name: String,
+    //@SerializedName("pactivos") val activePrinciples: String,
+    @SerializedName("labTitular") val laboratory: String,
+    @SerializedName("cpresc") val prescribingConditions: String,
+    @SerializedName("estado") val status: Status,
+    @SerializedName("comerc") val comerzialized: Boolean,
+    @SerializedName("receta") val needsRecipe: Boolean,
+    @SerializedName("generico") val isGeneric: Boolean,
+    @SerializedName("conduc") val affectsDriving: Boolean,
+    @SerializedName("triangulo") val hasBlackTriangule: Boolean,
+    @SerializedName("huerfano") val isOrphan: Boolean,
+    @SerializedName("biosimilar") val isBiosimilar: Boolean,
+    @SerializedName("nosustituible") val sustituibleItem: SustituibleItem,
+    @SerializedName("psum") val hasSupplyProblems: Boolean,
+    @SerializedName("notas") val hasNotes: Boolean,
+    @SerializedName("materialesInf") val hasComputerizedMaterials: Boolean,
+    @SerializedName("ema") val isEma: Boolean,
+    @SerializedName("docs") val documents: List<Document>?,
+    @SerializedName("fotos") val photos: List<Photo>?,
+    @SerializedName("viasAdministracion") val administrationType: List<AdministrationType>?,
+    @SerializedName("formaFarmaceutica") val dosageForm: DosageType,
+    @SerializedName("formaFarmaceuticaSimplificada") val simplifiedDosageForm: SimplifiedDosageType,
+    //@SerializedName("atcs") val atcCodes: List<AtcCode>?,
+    //@SerializedName("principiosActivos") val activePrinciplesList: List<activePrinciple>,
+    //@SerializedName("presentaciones") val presentation: List<Presentation>?,
+    @SerializedName("dosis") val dosis: String
+
+)
+
+/*data class AtcCode(
+    @SerializedName("codigo") val code: String,
+    @SerializedName("nombre") val name: String,
+    @SerializedName("nivel") val id: Int
+
+    )*/
+
+
+/*data class activePrinciple(
+    @SerializedName("id") val id: Int,
+    @SerializedName("codigo") val code: String,
+    @SerializedName("nombre") val name: String,
+    @SerializedName("cantidad") val quantity: String,
+    @SerializedName("unidad") val units: String,
+    @SerializedName("orden") val order: Int
+)*/
+
+
+/*
+data class Presentation(
+    @SerializedName("cn") val regNumber: String,
+    @SerializedName("nombre") val Name: String,
+    @SerializedName("estado") val status: Status,
+    @SerializedName("comerc") val isComercialized: Boolean,
+    @SerializedName("psum") val hasSupplyProblems: Boolean
+)
+*/
+
+data class Status(
+    @SerializedName("aut") val authorizedDate: Long,
+    )
+
+data class Document(
+    @SerializedName("tipo") val type: Int,
+    @SerializedName("url") val url: String,
+    @SerializedName("urlHtml") val urlHtml: String,
+    @SerializedName("secc") val isHtmlAvailable: Boolean,
+    @SerializedName("fecha") val docDate: Long
+)
+
+data class Photo(
+    @SerializedName("tipo") val type: String,
+    @SerializedName("url") val url: String,
+    //@SerializedName("fecha") val photoDate: Long
+
+)
+
+data class AdministrationType(
+    @SerializedName("id") val administrationId: Int,
+    @SerializedName("nombre") val administrationName: String
+)
+
+data class DosageType(
+    @SerializedName("id") val dosageId: Int,
+    @SerializedName("nombre") val dosageName: String
+)
+
+data class SimplifiedDosageType(
+    @SerializedName("id") val simplifiedDosageId: Int,
+    @SerializedName("nombre") val simplifiedDosageName: String
+)
+
+data class SustituibleItem(
+    @SerializedName("id") val id: Int?,
+    @SerializedName("nombre") val name: String,
+    @SerializedName("codigo") val code: String?
+)
